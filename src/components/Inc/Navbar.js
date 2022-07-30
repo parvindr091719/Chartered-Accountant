8f543843d5bc5559504533f9eaa94ff2f3d66068
Arvind Sharma <102983268+parvindr091719@users.noreply.github.com>
2022-07-30 17:59:53 +0530

    Update Navbar.js

--- src/components/Inc/Navbar.js
+++ src/components/Inc/Navbar.js
@@ -8,25 +8,25 @@ const Navbar = () => {
                 <div className="row">
                     <div className="col-md-12">
 
-                        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                            <div class="container-fluid">
+                        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                            <div className="container-fluid">
                                 <Link to="/" className="navbar-brand">Chartered Accountants</Link>
                                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                                    <span class="navbar-toggler-icon"></span>
+                                    <span className="navbar-toggler-icon"></span>
                                 </button>
-                                <div class="collapse navbar-collapse" id="navbarText">
-                                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                        <li class="nav-item">
+                                <div className="collapse navbar-collapse" id="navbarText">
+                                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                        <li className="nav-item">
                                             <Link to="/" className="nav-link active">Home</Link>
                                         </li>
-                                        <li class="nav-item">
+                                        <li className="nav-item">
                                             <Link to="/about" className="nav-link active">About Us</Link>
                                         </li>
-                                        <li class="nav-item">
+                                        <li className="nav-item">
                                             <Link to="/contact" className="nav-link active">Contact Us</Link>
                                         </li>
                                     </ul>
-                                    <span class="navbar-text text-white">
+                                    <span className="navbar-text text-white">
                                         Mr. Suraj Badhai
                                     </span> &npam
                                     <a target="_blank" id="action-list-11" class="dtKbfb  oYxtQd" rel="noreferrer" 
